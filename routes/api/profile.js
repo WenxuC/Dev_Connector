3a23b45fa4700566be66b43f46a0848ef588df3d
WenxuC <wenx.chen@outlook.com>
2022-01-04 18:45:27 -0500

    added delete experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -139,7 +139,7 @@ router.get('/user/:user_id', async (req, res) => {
 });
 
 // @route   DELETE api/profile
-// @desc    Delet profile, user & posts
+// @desc    Delete profile, user & posts
 // @access  Private
 router.delete('/', auth, async (req, res) => {
 	try {
@@ -151,6 +151,72 @@ router.delete('/', auth, async (req, res) => {
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send('Server Error');
+	}
+});
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+	'/experience',
+	[
+		auth,
+		[
+			check('title', 'Title is required').not().isEmpty(),
+			check('company', 'Company is required').not().isEmpty(),
+			check('from', 'From date is required').not().isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { title, company, location, from, to, current, description } =
+			req.body;
+
+		const newExp = {
+			title,
+			company,
+			location,
+			from,
+			to,
+			current,
+			description,
+		};
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+
+			profile.experience.unshift(newExp);
+
+			await profile.save();
+
+			res.json(profile);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+	try {
+		const foundProfile = await Profile.findOne({ user: req.user.id });
+
+		foundProfile.experience = foundProfile.experience.filter(
+			exp => exp._id.toString() !== req.params.exp_id
+		);
+
+		await foundProfile.save();
+		return res.status(200).json(foundProfile);
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ msg: 'Server error' });
 	}
 });
 
