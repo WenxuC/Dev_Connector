83f692187feafbfeb6f0747cf3f7627ddeb96924
WenxuC <wenx.chen@outlook.com>
2022-01-12 11:00:13 -0500

    fixed alert-wrapper

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -24,17 +24,13 @@ const App = () => {
 	return (
 		<Provider store={store}>
 			<Router>
-				<Fragment>
-					<Navbar />
-					<section className='container'>
-						<Alert />
-						<Routes>
-							<Route path='/' element={<Landing />} />
-							<Route path='/register' element={<Register />} />
-							<Route path='/login' element={<Login />} />
-						</Routes>
-					</section>
-				</Fragment>
+				<Navbar />
+				<Alert />
+				<Routes>
+					<Route path='/' element={<Landing />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/login' element={<Login />} />
+				</Routes>
 			</Router>
 		</Provider>
 	);
