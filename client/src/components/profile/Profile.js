0237a2dd4080e82068d90ddc472c283c756f9031
WenxuC <wenx.chen@outlook.com>
2022-01-18 19:24:11 -0500

    added profile experience and education

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience.js';
+import ProfileEducation from './ProfileEducation.js';
+// import ProfileGithub from './ProfileGithub.js';
 import { connect } from 'react-redux';
 import { getProfileById } from '../../actions/profile';
 import { useParams, Link } from 'react-router-dom';
@@ -13,14 +16,13 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
 		getProfileById(id);
 	}, [getProfileById, id]);
 	return (
-		<Fragment>
-			{profile === null || loading ? (
+		<section className='container'>
+			{profile === null ? (
 				<Spinner />
 			) : (
 				<Fragment>
 					<Link to='/profiles' className='btn btn-light'>
-						{' '}
-						Back to profiles
+						Back To Profiles
 					</Link>
 					{auth.isAuthenticated &&
 						auth.loading === false &&
@@ -29,13 +31,49 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth }) => {
 								Edit Profile
 							</Link>
 						)}
-					<div class='profile-grid my-1'>
+					<div className='profile-grid my-1'>
 						<ProfileTop profile={profile} />
 						<ProfileAbout profile={profile} />
+						<div className='profile-exp bg-white p-2'>
+							<h2 className='text-primary'>Experience</h2>
+							{profile.experience.length > 0 ? (
+								<Fragment>
+									{profile.experience.map(experience => (
+										<ProfileExperience
+											key={experience._id}
+											experience={experience}
+										/>
+									))}
+								</Fragment>
+							) : (
+								<h4>No experience credentials</h4>
+							)}
+						</div>
+
+						<div className='profile-edu bg-white p-2'>
+							<h2 className='text-primary'>Education</h2>
+							{profile.education.length > 0 ? (
+								<Fragment>
+									{profile.education.map(education => (
+										<ProfileEducation
+											key={education._id}
+											education={education}
+										/>
+									))}
+								</Fragment>
+							) : (
+								<h4>No education credentials</h4>
+							)}
+						</div>
+
+						{/* Getting 404 errors when retrieveing repos */}
+						{/* {profile.githubusername && (
+							<ProfileGithub username={profile.githubusername} />
+						)} */}
 					</div>
 				</Fragment>
 			)}
-		</Fragment>
+		</section>
 	);
 };
 
