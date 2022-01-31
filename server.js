edf9a3ddcec2bece49f6b286e2873935289ac842
WenxuC <wenx.chen@outlook.com>
2022-01-31 16:41:46 -0500

    .

--- server.js
+++ server.js
@@ -22,7 +22,10 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		const here = res.sendFile(
+			path.resolve(__dirname, 'client', 'build', 'index.html')
+		);
+		console.log('here', here);
 	});
 }
 const PORT = process.env.PORT || 5000;
